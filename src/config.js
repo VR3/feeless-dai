218683c35638c618cc84e2593bce056ca440e374
Irving Cabello <irvollo@gmail.com>
2018-10-18 13:24:01 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    apiKey: "af78048d2db145cab278b3986a9d5754",
-    mnemonic: "cactus crane have symptom calm vehicle gas tip neglect street hood caught",
-    privateKey: "ac8d660de610a924d05feb535491a6ea898eedd8696afb6b06f19c7e3a48550c"
+    apiKey: "YOUR_API_KEY_HERE",
+    mnemonic: "YOUR_OWN_MNEMONIC",
+    privateKey: "YOUR_PRIVATE_KEY"
 }
